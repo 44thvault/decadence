6fb67927e9f0df8d415f17ea74d329fec0ac79f9
44thvault <44thvault@gmail.com>
2026-06-11 01:24:06 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import DecadenceGame from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DecadenceGame />
+    <Analytics />
   </React.StrictMode>
 )
